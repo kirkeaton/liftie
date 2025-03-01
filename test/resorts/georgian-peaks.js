b3a35685d905a28ae901d15cd533af7a25dc3583
Kirk Eaton <contact@kirkeaton.ca>
2025-03-01 07:51:50 -0500

    Add Georgian Peaks resort

---
+++ test/resorts/georgian-peaks.js
@@ -0,0 +1,29 @@
+const lifts = require('../lifts');
+
+lifts('georgian-peaks', 'html', {
+  Champlain: 'open',
+  'Bay Street': 'open',
+  "Gammell's (Chimo)": 'open',
+  "Roger's Run": 'open',
+  'Slalom': 'open',
+  'Minute Mile': 'open',
+  'Riot': 'open',
+  'Race Grade': 'open',
+  'Orchard': 'open',
+  'Thunder': 'open',
+  'Tomahawk Glade': 'open',
+  'Sun Up': 'open',
+  'Thunderhead': 'open',
+  "Peggy's Run": 'open',
+  'Freeloader': 'open',
+  'Monster': 'open',
+  'Outer Rim': 'open',
+  'Rampage': 'open',
+  "Wilson's Wiggle": 'open',
+  'Lightning': 'open',
+  'Snow Bowl': 'open',
+  'Champlain Terrain Park': 'open',
+  'Hiking Trail': 'open',
+  'Skinning Trail': 'open',
+  'Skating Rink': 'open'
+});
